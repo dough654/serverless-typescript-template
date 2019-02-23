bc6ea59959a722a392469dad070e5d047616f1b2
Doug Gerhardt <douglaswgerhardt@gmail.com>
2019-02-23 12:51:08 -0500

    Added start script to package.json

--- src/getHello.ts
+++ src/getHello.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  console.log('hello')
   return {
     statusCode: 200,
     body: JSON.stringify({
--- src/postHello.ts
+++ src/postHello.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const run: APIGatewayProxyHandler = async (event, _context) => {
+  console.log('i ran')
   return {
     statusCode: 200,
     body: JSON.stringify({
